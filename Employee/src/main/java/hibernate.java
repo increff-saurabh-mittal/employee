a67e744a48caae3056e3d8701e47267748b809e7
Saurabh Mittal <saurabhmittal@N0373-Saurabh.local>
2022-11-20 19:40:12 +0530

    maven

---
+++ Employee/src/main/java/hibernate.java
@@ -0,0 +1,32 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class hibernate {
+    private static final Logger logger = Logger.getLogger(String.valueOf(Main.class));
+
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+
+        Properties props = new Properties();
+        InputStream inputStream  = new FileInputStream("employee.properties");
+        props.load(inputStream);
+
+        Class.forName(props.getProperty("jdbc.className"));
+        Connection con= DriverManager.getConnection(
+                props.getProperty("jdbc.url"),props.getProperty("jdbc.user"),props.getProperty("jdbc.password"));
+
+        Statement stmt=con.createStatement();
+        ResultSet rs=stmt.executeQuery("select * from employee");
+
+        while(rs.next())
+            logger.info(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getInt(3));
+        con.close();
+
+
+        logger.info("Hello world!");
+    }
+}
